963ae8e8ed9d1b01d5f37f1481d6a641d719787f
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-17 22:31:08 +0530

    Added:to test annotation

---
+++ src/main/java/MethodInfo.java
@@ -0,0 +1,15 @@
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodInfo {
+    String author() default "himanshu";
+
+    String date();
+
+    int revision() default 1;
+
+    String comments();
+}
